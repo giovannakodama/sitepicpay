baf975f7a193d8db9b5c0bbecc2a788338c176a2
Tiago Grossi Hasuda <tiago.ghasuda@gmail.com>
2021-11-12 12:24:47 -0300

    fix: login

--- user/script.js
+++ user/script.js
@@ -1,6 +1,4 @@
 (() => {
     const cookies = document.cookie
-    console.log(cookies)
     if (!cookies) return location.replace('../index.html');
-    if (!cookies.startsWith('email=')) return location.replace('../index.html');
 })()
